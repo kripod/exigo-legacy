f12d565e3f1b4e32a25419d93635a09fb9119299
Kristóf Poduszló <kripod@protonmail.com>
2019-02-22 23:21:27 +0100

    Generate assessment pages dynamically

---
+++ gatsby-node.js
@@ -0,0 +1,41 @@
+const { createFilePath } = require('gatsby-source-filesystem');
+const { resolve } = require('path');
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === 'Json') {
+    const slug = createFilePath({ node, getNode, basePath: 'pages' });
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug,
+    });
+  }
+};
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  return graphql(`
+    {
+      allJson {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allJson.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: resolve('./src/templates/quiz.tsx'),
+        context: {
+          slug: node.fields.slug,
+        },
+      });
+    });
+  });
+};
---
+++ src/templates/quiz.tsx
@@ -0,0 +1,52 @@
+import { graphql } from 'gatsby';
+import shuffle from 'lodash.shuffle';
+import React from 'react';
+import { Container } from 'reactstrap';
+import Assessment from '../components/Assessment';
+import Layout from '../components/Layout';
+import Measure from '../components/Measure';
+
+type Props = {
+  data: any;
+};
+
+export default function QuizTemplate({ data }: Props) {
+  const shuffledAssessments = shuffle(data.json.assessments);
+
+  return (
+    <Layout>
+      <Container tag={Measure}>
+        {/* TODO: Read title from JSON */}
+        <h2>Makroökonómia kvíz</h2>
+
+        {shuffledAssessments.map((assessment, i) => (
+          <Assessment
+            key={
+              // The list of assessments will not ever change, so we're safe
+              // eslint-disable-next-line react/no-array-index-key
+              i
+            }
+            question={assessment.question}
+            correctAnswer={assessment.correctAnswer ? 'Igaz' : 'Hamis'}
+            incorrectAnswerChoices={[
+              assessment.correctAnswer ? 'Hamis' : 'Igaz',
+            ]}
+            className="mt-3 mb-4"
+          />
+        ))}
+      </Container>
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    json(fields: { slug: { eq: $slug } }) {
+      id
+      assessments {
+        question
+        correctAnswer
+      }
+    }
+  }
+`;
