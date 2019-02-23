d68da02fd5fc923522c603706a0c107e1a812f69
Kristóf Poduszló <kripod@protonmail.com>
2019-02-23 11:06:25 +0100

    List quizzes on the front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,42 @@
+import { graphql, Link } from 'gatsby';
 import React from 'react';
+import { Container } from 'reactstrap';
 import Layout from '../components/Layout';
 
-export default function IndexPage() {
+type Props = {
+  data: any;
+};
+
+export default function IndexPage({ data }: Props) {
   return (
     <Layout>
-      <p>Hello, world!</p>
+      <Container>
+        <h2>Quizzes</h2>
+
+        <ul>
+          {data.allJson.edges.map(({ node }: any) => (
+            <li key={node.id}>
+              <Link to={node.fields.slug}>{node.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </Container>
     </Layout>
   );
 }
+
+export const query = graphql`
+  {
+    allJson {
+      edges {
+        node {
+          id
+          title
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`;
